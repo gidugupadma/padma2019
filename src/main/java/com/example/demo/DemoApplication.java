17ec33b08c280db550ca5d7ae513742e6e97830a
gidugupadma <gidugupadma@gmail.com>
2019-05-09 17:57:30 +0530

    changes in demoapplicaton java class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		
-		System.out.println();
+		System.out.println("demo application for profiles in spring boot");
 	}
    
 	
