c6d30de74ad724607c17f6de2a33af760b0c432c
gidugupadma <gidugupadma@gmail.com>
2019-05-09 17:24:17 +0530

    now its intial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		
+		System.out.println();
+	}
+   
+	
+	@Profile("dev")
+	@Bean
+	public String devApp(){
+		System.out.println("ur in dev environment");
+		return "I will be available in profile dev";
+	}
+	
+	
+	@Profile("qa")
+	@Bean
+	public String qaApp(){
+		System.out.println("ur in qa environment");
+		return "I will be available in profile QA";
+	}
+}
