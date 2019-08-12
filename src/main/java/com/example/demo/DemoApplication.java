8b4b2d08b5b01f9273e937ad398529a96a3dacbc
padmagidugu <padma.g920@gmail.com>
2019-08-12 15:42:48 +0530

    Test padma.g920 commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		
+		System.out.println("changes in padma.g920");
 		System.out.println("demo application for profiles in spring boot");
 		
 	}
