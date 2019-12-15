a2ce6020101ae3c4a6a6d44d4ac1641eb195964f
padmagidugu <padma.g920@gmail.com>
2019-12-15 08:52:56 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -33,3 +33,88 @@ public class DemoApplication {
 		return "I will be available in profile QA";
 	}
 }
+
+package com.example.demo;
+
+import java.io.*;
+import java.util.*;
+
+public class Test {
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		File file = new File("C:/Users/Lenovo/Desktop/Padma CTS/parser.txt");
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String dataString = "";
+		String st;
+		Map<String, String> dataListBin1 = new HashMap<String, String>();
+		Map<String, String> dataListBin2 = new HashMap<String, String>();
+		while ((st = br.readLine()) != null) {
+			if (!st.equals("")) {
+				dataString = st;
+			}
+		}
+		String[] arr = dataString.split(" Current bin");
+		String sttt1 = arr[1];
+		String sttt2 = arr[2];
+
+		String[] arrb1 = sttt1.split("\\\\r\\\\n");
+		String[] arrb2 = sttt2.split("\\\\r\\\\n");
+		
+		for (String stt : arrb1) {
+			if (!stt.isEmpty() && !stt.equals("")) {
+				String[] stslip = stt.split("\\|");
+				// System.out.println(stslip.length);
+				if (stslip.length == 3) {
+					String key = stslip[1].trim();
+					String value = stslip[2].trim();
+					//System.out.println(key + "        " + value);
+					if (key.equalsIgnoreCase("Parameter")
+							&& value.equalsIgnoreCase("Value"))
+						break;
+					switch (key) {
+					case "RxPkts":
+						dataListBin1.put("RxPkts", value);
+						break;
+					case "RxDropPkts":
+						dataListBin1.put("RxDropPkts", value);
+						break;
+					case "TxCrcErrorPkts":
+						dataListBin1.put("TxCrcErrorPkts", value);
+						break;
+					}
+				}// end length
+			}//end of if
+		}//end of Bin1
+		
+		for (String stt : arrb2) {
+			if (!stt.isEmpty() && !stt.equals("")) {
+				String[] stslip = stt.split("\\|");
+				// System.out.println(stslip.length);
+				if (stslip.length == 3) {
+					String key = stslip[1].trim();
+					String value = stslip[2].trim();
+					//System.out.println(key + "        " + value);
+					if (key.equalsIgnoreCase("Parameter")
+							&& value.equalsIgnoreCase("Value"))
+						break;
+					switch (key) {
+					case "RxPkts":
+						dataListBin2.put("RxPkts", value);
+						break;
+					case "RxDropPkts":
+						dataListBin2.put("RxDropPkts", value);
+						break;
+					case "TxCrcErrorPkts":
+						dataListBin2.put("TxCrcErrorPkts", value);
+						break;
+					}
+				}// end length
+			}//end of if
+		}//end of Bin1
+		
+		System.out.println(dataListBin1);
+		System.out.println(dataListBin2);
+	}
+	
+}
+
